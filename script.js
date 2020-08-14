8b9e1805302682af5d86973e2523760149dc098b
Marcin-Popek <64362635+Marcin-Popek@users.noreply.github.com>
2020-08-14 14:40:41 +0200

    działające menu i korekty stylowe tekstu

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const brg = document.querySelector('.fas');
 const bars = document.querySelector('.fa-bars');
 const arw = document.querySelector('.fa-arrow-left');
 const mnu = document.querySelector('.menu');
+var mgr = 25;
 console.log(brg);
 bars.addEventListener('click', function () {
      bars.classList.toggle("on");
@@ -15,10 +16,12 @@ arw.addEventListener('click', function () {
      mnu.classList.toggle("on");
      console.log('klik!');
 });
-
+if ($(document).width() <= 1024) {
+     mgr = 0;
+}
 $('.s1').on('click', function () {
      $('body, html').animate({
-          scrollTop: $('.welcome').offset().top
+          scrollTop: $('.welcome').offset().top - mgr
      }, 500)
      $('.menu').toggleClass("on");
      $('.fa-arrow-left').toggleClass("on");
@@ -26,7 +29,7 @@ $('.s1').on('click', function () {
 })
 $('.s2').on('click', function () {
      $('body, html').animate({
-          scrollTop: $('.discover').offset().top - 50
+          scrollTop: $('.discover').offset().top - mgr
      }, 500)
      $('.menu').toggleClass("on");
      $('.fa-arrow-left').toggleClass("on");
@@ -34,7 +37,7 @@ $('.s2').on('click', function () {
 })
 $('.s3').on('click', function () {
      $('body, html').animate({
-          scrollTop: $('.m2').offset().top
+          scrollTop: $('.m2').offset().top - mgr
      }, 500)
      $('.menu').toggleClass("on");
      $('.fa-arrow-left').toggleClass("on");
@@ -42,7 +45,7 @@ $('.s3').on('click', function () {
 })
 $('.s4').on('click', function () {
      $('body, html').animate({
-          scrollTop: $('.map').offset().top
+          scrollTop: $('.map').offset().top - mgr
      }, 500)
      $('.menu').toggleClass("on");
      $('.fa-arrow-left').toggleClass("on");
@@ -50,7 +53,7 @@ $('.s4').on('click', function () {
 })
 $('.s5').on('click', function () {
      $('body, html').animate({
-          scrollTop: $('.contact').offset().top
+          scrollTop: $('.contact').offset().top - mgr
      }, 500)
      $('.menu').toggleClass("on");
      $('.fa-arrow-left').toggleClass("on");
